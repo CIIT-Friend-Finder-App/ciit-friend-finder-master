cf6e7725ee5450eb929693273185b8bf6ee0cd3e
danapalania <dana.p.alania@ciit.edu.ph>
2025-07-24 22:13:21 +0800

    profile preview update

--- app/ProfilePreview.js
+++ app/ProfilePreview.js
@@ -1,4 +1,4 @@
-import { View, ScrollView, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Text, Pressable } from 'react-native';
 import { useState } from 'react';
 import { createNativeWrapper } from 'react-native-gesture-handler';
 
@@ -8,9 +8,52 @@ export default function ProfilePreview({ goTo }) {
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style = {styles.header}>
                     <Text style = {styles.heading}>Profile Preview</Text>
-                    <Text style = {styles.subheading}>Let other users get to know you!</Text>
+                    <Text style = {styles.subheading}>Here's what you'll look like to other users</Text>
                     <View style = {styles.card}>
-                        
+                        <Text style = {styles.nickname}>Nickname, <Text style = {styles.age}>19</Text></Text>
+                        <View style = {styles.bioContain}>
+                            <Text style = {styles.bio}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </Text>
+                        </View>
+                        <Text style = {styles.labels}>About Me</Text>
+                        <View style = {styles.aboutmecontain}>
+                            <View style = {styles.aboutmeparent}>
+                                <Text style = {styles.aboutmechild}>She/Her</Text>
+                            </View>
+                            <View style = {styles.aboutmeparent}>
+                                <Text style = {styles.aboutmechild}>Second Year</Text>
+                            </View>
+                            <View style = {styles.aboutmeparent}>
+                                <Text style = {styles.aboutmechild}>BMMA - Graphic Design</Text>
+                            </View>
+                        </View>
+                        <View style = {styles.bioContain}>
+                            <Text style = {styles.labels}>Interests</Text>
+                            <Text style = {styles.bio2}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </Text>
+                        </View>
+                        <View style = {styles.bioContain}>
+                            <Text style = {styles.labels}>Hobbies</Text>
+                            <Text style = {styles.bio2}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </Text>
+                        </View>
+                        <View style = {styles.bioContain}>
+                            <Text style = {styles.labels}>Looking For</Text>
+                            <Text style = {styles.bio2}>
+                                Looking for a study buddy!
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View style = {styles.colcontainer}>
+                    <View style = {styles.column}>
+                        <Pressable style = {styles.button} onPress={() => goTo('createprofile')} ><Text style = {styles.buttontext}>Edit</Text></Pressable>
+                    </View>
+                    <View style = {styles.column}>
+                        <Pressable style = {styles.button} onPress={() => goTo('profilepreview')} ><Text style = {styles.buttontext}>Finish Profile</Text></Pressable>
                     </View>
                 </View>
             </ScrollView>
@@ -51,12 +94,15 @@ const styles = StyleSheet.create({
 
     card: {
         width: 320,
-        backgroundColor: 'white',
+        backgroundColor: '#FFD6EA',
         margin: 10,
         alignSelf: 'center',
         borderRadius: 10,
+        borderWidth: 3,
+        borderColor: '#B3B3B3',
+        borderStyle: 'solid',
         padding: 10,
-        shadowColor: "black",
+        shadowColor: 'black',
         shadowOffset: {
             width: 0,
             height: 2,
@@ -64,6 +110,102 @@ const styles = StyleSheet.create({
         shadowOpacity:  0.17,
         shadowRadius: 2.54,
         elevation: 3
-    }
+    },
+
+    nickname: {
+        textAlign: 'center',
+        fontFamily: 'HelveticaNeueHeavy',
+        color: '#B3B3B3',
+        fontSize: 20,
+        paddingLeft: 5,
+        paddingTop: 2,
+    },
+
+    age: {
+        fontSize: 14,
+        fontFamily: 'HelveticaNeueHeavy',
+    },
+
+    bioContain: {
+        marginTop: 8 ,
+        paddingBottom: 4,
+        minHeight: 10,
+        borderRadius: 7,
+        width: 295,
+        backgroundColor: '#ebebebff',
+    },
+
+    bio: {
+        fontFamily: 'HelveticaNeueRoman',
+        color: '#B3B3B3',
+        fontSize: 12, 
+        paddingTop: 6,
+        paddingLeft: 2,
+        paddingRight: 2,
+        textAlign: 'center',
+        lineHeight: 16,
+    },
+
+    labels: {
+        fontFamily: 'HelveticaNeueHeavy',
+        color: '#B3B3B3',
+        fontSize: 20,
+        paddingLeft: 5,
+        paddingTop: 10,
+    },
+
+    aboutmecontain: {
+        width: 295,
+        flexDirection: 'row', 
+        flexWrap: 'wrap',
+    },
+
+    aboutmeparent: {
+        padding: 10,
+        borderRadius: 18,
+        marginLeft: 5,
+        marginBottom: 10,
+        backgroundColor: '#ebebebff',
+    },
+
+    aboutmechild: {
+        fontFamily: 'HelveticaNeueHeavy',
+        color: '#B3B3B3',
+    },
+
+    bio2: {
+        fontFamily: 'HelveticaNeueRoman',
+        color: '#B3B3B3',
+        fontSize: 12, 
+        paddingTop: 6,
+        paddingLeft: 10,
+        paddingRight: 2,
+        lineHeight: 16,
+    },
+
+    /* buttons */
+
+    colcontainer: {
+        flexDirection: 'row',
+        flex: 1,
+    },
+    column: {
+        flex: 1,
+    },
+
+    button: {
+        width: '90%',
+        backgroundColor: '#F82E4B',
+        marginTop: 20,
+        borderRadius: 30,
+        padding: 10,
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    buttontext: {
+        color: 'white',
+        fontFamily: 'HelveticaNeueRoman',
+        fontSize: 13
+    },
     
 });
